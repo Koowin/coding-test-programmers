d32ed674a35bf3e9959ce3d990808675d6dd3a7a
Koowin <koowin42@gmail.com>
2022-01-23 13:39:48 +0900

    add: S17680

--- src/com/Main.java
+++ src/com/Main.java
@@ -8,8 +8,25 @@ public class Main {
         String input = "";
         //printBracketToBrace(input);
 
-        S43165 s = new S43165();
-        System.out.println(s.solution(new int[] {1, 1, 1, 1, 1}, 3));
+        S17680 s = new S17680();
+        String[][] testCases = new String[6][];
+        testCases[0] = new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        testCases[1] = new String[] {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+        testCases[2] = new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        testCases[3] = new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+        testCases[4] = new String[] {"Jeju", "Pangyo", "NewYork", "newyork"};
+        testCases[5] = new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+
+        int[] cacheSizes = new int[6];
+        cacheSizes[0] = 3;
+        cacheSizes[1] = 3;
+        cacheSizes[2] = 2;
+        cacheSizes[3] = 5;
+        cacheSizes[4] = 2;
+        cacheSizes[5] = 0;
+        for(int i=0, size = cacheSizes.length;i<size;i++){
+            System.out.println(s.solution(cacheSizes[i], testCases[i]));
+        }
     }
 
     private static void printBracketToBrace(String input){
--- src/com/Test.java
+++ src/com/Test.java
@@ -4,12 +4,10 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(13);
-        list.add(1);
-        list.add(4);
-        list.add(0, 1231412);
-        System.out.println(list);
+        Integer a = new Integer(123);
+        System.out.println(a.hashCode());
+        a = 456;
+        System.out.println(a.getClass());
 
     }
 }
