21c1e36c820e9b73478288a50430f5fc347bf0e6
Koowin <koowin42@gmail.com>
2022-02-08 16:12:42 +0900

    Update S86052.java

--- src/two/S86052.java
+++ src/two/S86052.java
@@ -1,5 +1,7 @@
 package two;
 
+import java.util.ArrayList;
+
 public class S86052 {
 
 
@@ -10,11 +12,24 @@ class Grid{
     private static final int RIGHT = 1;
     private static final int LEFT = 2;
     private final int[][] modules;
-    
+
+    private boolean [][] top;
+    private boolean [][] bottom;
+    private boolean [][] left;
+    private boolean [][] right;
+    private ArrayList<Integer> answer = new ArrayList<>();
+
     public Grid(String[] str){
         int rowSize = str.length;
         int columnSize = str[0].length();
+
         modules = new int[rowSize][columnSize];
+
+        top = new boolean[rowSize][columnSize];
+        bottom = new boolean[rowSize][columnSize];
+        left = new boolean[rowSize][columnSize];
+        right = new boolean[rowSize][columnSize];
+
         for(int i=0;i<rowSize;i++){
             String s = str[i];
             for(int j=0;j<columnSize;j++){
@@ -33,5 +48,13 @@ class Grid{
                 }
             }
         }
+    }
+
+    public void shootAll(){
+
+    }
+
+    private void shoot(){
+
     }
 }
