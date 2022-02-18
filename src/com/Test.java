db2c4b52700409200dc514ce3a4970469f08183f
Koowin <koowin42@gmail.com>
2022-02-18 17:54:46 +0900

    add: S42587

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,5 +4,18 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        String[] abc = new String[] {"119", "97674223", "1195524421"};
+        Arrays.sort(abc, (o1, o2) -> {
+            if (o1.length() > o2.length()) {
+                return 1;
+            } else if (o1.length() < o2.length()) {
+                return -1;
+            }
+            return 0;
+        });
+
+        for(String s : abc){
+            System.out.println(s);
+        }
     }
 }
