fc88e5cd6bf1b750a6b51d914e21cd5424fe0c55
Koowin <56014482+Koowin@users.noreply.github.com>
2022-04-11 10:32:06 +0900

    add: S81303

--- src/three/S81303.java
+++ src/three/S81303.java
@@ -3,105 +3,127 @@ package three;
 import java.util.*;
 
 public class S81303 {
-    public static void main(String[] args) {
-        S81303 s = new S81303();
-        System.out.println(s.solution(8, 2, new String[] {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"}));
-    }
+    private Row cursor;
+    Deque<Row> deleteStack = new LinkedList<>();
 
     public String solution(int n, int k, String[] cmd) {
-        TableEditor tableEditor = new TableEditor(n, k);
+        makeLinkedRow(n);
+        down(k);
+
         for (String command : cmd) {
-            tableEditor.executeCommand(command);
+            executeCommand(command);
         }
 
-        return tableEditor.toString();
+        return rowToString(n);
     }
 
-    static class TableEditor {
-        private boolean[] records;
-        private int index;
-        private int lastIndex;
-        private Deque<Integer> deleteStack = new LinkedList<>();
+    private void makeLinkedRow(int n) {
+        cursor = new Row(0);
+        Row r = cursor;
+        for (int i = 1; i < n; i++) {
+            Row newRow = new Row(i);
+            r.add(newRow);
+            r = newRow;
+        }
+    }
 
-        private TableEditor(int n, int k) {
-            records = new boolean[n];
-            for (int i = 0; i < n; i++) {
-                records[i] = true;
-            }
-            index = k;
-            lastIndex = n - 1;
+    private void executeCommand(String command) {
+        char c = command.charAt(0);
+        switch(c){
+            case 'U':
+                up(Integer.parseInt(command.substring(2)));
+                break;
+            case 'D':
+                down(Integer.parseInt(command.substring(2)));
+                break;
+            case 'C':
+                delete();
+                break;
+            case 'Z':
+                undo();
+                break;
+        }
+    }
+
+    private void up(int len) {
+        for (int i = 0; i < len; i++) {
+            cursor = cursor.prev;
+        }
+    }
+
+    private void down(int len) {
+        for (int i = 0; i < len; i++) {
+            cursor = cursor.next;
+        }
+    }
+
+    private void delete() {
+        deleteStack.push(cursor);
+        cursor = cursor.delete();
+    }
+
+    private void undo() {
+        Row r = deleteStack.pop();
+        r.undo();
+    }
+
+    public String rowToString(int n) {
+        StringBuilder stringBuilder = new StringBuilder(n);
+        while (cursor.prev != null) {
+            cursor = cursor.prev;
         }
 
-        private void executeCommand(String command) {
-            if (command.length() != 1) {
-                int count = Integer.parseInt(command.substring(2, command.length()));
-                if (command.charAt(0) == 'D') {
-                    downIndex(count);
-                } else {
-                    upIndex(count);
-                }
+        char O = 'O';
+        char X = 'X';
+
+        for (int i = 0; i < n; i++) {
+            if (cursor != null && i == cursor.index) {
+                cursor = cursor.next;
+                stringBuilder.append(O);
             } else {
-                if (command.charAt(0) == 'C') {
-                    delete();
-                } else {
-                    undoLastDelete();
-                }
+                stringBuilder.append(X);
             }
         }
+        return stringBuilder.toString();
+    }
 
-        private void downIndex(int count) {
-            while (count > 0) {
-                if (records[++index]) {
-                    count--;
-                }
-            }
+    static class Row {
+        private final int index;
+        private Row prev;
+        private Row next;
+
+        private Row(int index) {
+            this.index = index;
         }
 
-        private void upIndex(int count) {
-            while (count > 0) {
-                if (records[--index]) {
-                    count--;
-                }
-            }
+        private void add(Row r) {
+            next = r;
+            r.prev = this;
         }
 
-        private void delete() {
-            records[index] = false;
-            deleteStack.push(index);
-
-            if (index == lastIndex) {
-                while (records[--lastIndex]);
-                index = lastIndex;
+        private Row delete() {
+            Row ret;
+            if (prev == null) {
+                ret = next;
+                next.prev = null;
+            } else if (next == null) {
+                ret = prev;
+                prev.next = null;
+            } else {
+                ret = next;
+                prev.next = next;
+                next.prev = prev;
             }
-            else {
-                downIndex(1);
-            }
+            return ret;
         }
 
-        private void undoLastDelete() {
-            int lastDeletedIndex = deleteStack.pop();
-            records[lastDeletedIndex] = true;
-            if (lastDeletedIndex > lastIndex) {
-                lastIndex = lastDeletedIndex;
+        private void undo() {
+            if (prev != null) {
+                prev.next = this;
             }
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder ret = new StringBuilder();
-
-            char trueChar = 'O';
-            char falseChar = 'X';
-
-            for (boolean record : records) {
-                if (record) {
-                    ret.append(trueChar);
-                } else {
-                    ret.append(falseChar);
-                }
+            if (next != null) {
+                next.prev = this;
             }
-
-            return ret.toString();
         }
     }
 }
