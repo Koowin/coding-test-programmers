97f04ebcfa8313c7b476a260e390827f3d066a1e
Koowin <56014482+Koowin@users.noreply.github.com>
2022-05-14 12:04:50 +0900

    update: gitignore

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,7 +5,7 @@ import two.*;
 
 public class Main {
     public static void main(String[] args) {
-        printBracketToBrace("[[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]");
+        printBracketToBrace("[[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]");
     }
 
     private static void printBracketToBrace(String input){
